347c5a18c756a83419959dc8ad778c1eb1e58f83
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-06-27 06:26:40 +0530

    login ui done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    component:LandingComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'sign-up',
+    component:SignUpComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
