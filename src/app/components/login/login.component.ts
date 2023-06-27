1d2331d61cc334484b6017113476d92c4f6c2edc
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-06-28 04:42:53 +0530

    login not working

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,24 +1,56 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  loginForm=new FormGroup({
-    email:new FormControl('',Validators.compose([Validators.required,Validators.email])),
-    password:new FormControl('',Validators.required),
-  })
+  constructor(
+    private authService: AuthenticationService,
+    private toast: HotToastService,
+    private router: Router,
+    private fb: NonNullableFormBuilder
+  ) {}
 
-  get email(){
+  ngOnInit(): void {
+  }
+
+  get email() {
     return this.loginForm.get('email');
   }
 
-  get password(){
+  get password() {
     return this.loginForm.get('password');
   }
 
+  public submit() {
+    const { email, password } = this.loginForm.value;
+
+    if (!this.loginForm.valid || !email || !password) {
+      return;
+    }
+
+    this.authService
+      .login(email, password)
+      .pipe(
+        this.toast.observe({
+          success: 'Logged in successfully',
+          loading: 'Logging in...',
+          error: ({ message }) => `There was an error: ${message} `,
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+  }
 }
