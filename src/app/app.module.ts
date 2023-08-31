f86ce6156768cd5478e03953fd0c10e5b499fe07
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-08-31 21:22:51 +0530

    signup and route guards done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
@@ -55,7 +56,8 @@ import { HotToastModule } from '@ngneat/hot-toast';
     provideRemoteConfig(() => getRemoteConfig()),
     provideStorage(() => getStorage()),
     ReactiveFormsModule,
-    HotToastModule.forRoot()
+    HotToastModule.forRoot(),
+    MatMenuModule
   ],
   providers: [
     ScreenTrackingService,UserTrackingService
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+export function passwordsMatchValidator():ValidatorFn{
+  return (control:AbstractControl):ValidationErrors|null=>{
+    const password =control.get('password')?.value;
+    const confirmPassword=control.get('confirmPassword')?.value
+
+    if(password&&confirmPassword&& password!==confirmPassword){
+      return{
+        passwordDontMatch:true
+      }
+    }
+
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +25,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+
+  constructor(private auth:AuthenticationService,private toast:HotToastService, private router:Router){
+
+  }
+
+  signUpForm= new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.email,Validators.required]),
+    password:new FormControl('',[Validators.required]),
+    confirmPassword:new FormControl('',Validators.required),
+  },{validators:passwordsMatchValidator()})
+
+  get name(){
+    return this.signUpForm.get('name')
+  }
+
+  get email(){
+    return this.signUpForm.get('email')
+  }
+
+  get password(){
+    return this.signUpForm.get('password')
+  }
+
+  get confirmPassword(){
+    return this.signUpForm.get('confirmPassword')
+  }
+
+  signUp(){
+    const { name, email, password}=this.signUpForm.value;
+
+    if(!this.signUpForm.valid||!name||!email||!password) return;
+    this.auth.signUp(name,email,password).pipe(
+      this.toast.observe({
+        success:'Signed Up',
+        loading:'Signing in',
+        error:({message})=>`${message}`
+      })
+    ).subscribe(()=>{
+      this.router.navigate(['/home']);
+    })
+  }
 
 }
